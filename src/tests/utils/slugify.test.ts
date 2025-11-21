2ee19543c259caddf38f2da326c6fcbfd8bb7479
Kevin Lin <kl2806@columbia.edu>
2025-11-21 14:44:22 -0800

    chore: remove unintended files from branch (tests, eval, settings, subrepo)

--- src/tests/utils/slugify.test.ts
+++
@@ -1,9 +0,0 @@
-import { slugify } from "../../utils/slugify";
-
-test("slugify lowercases and hyphenates", () => {
-  expect(slugify("Hello World Test")).toBe("hello-world-test");
-});
-
-test("slugify removes accents and punctuation", () => {
-  expect(slugify("Crème brûlée!")).toBe("creme-brulee");
-});
